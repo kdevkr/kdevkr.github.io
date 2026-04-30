786f92a15544f637a9031edf90d17e51cb2ed2e5
kdevkr <kdevkr@users.noreply.github.com>
2026-05-01 08:00:03 +0900

    deploy: 7dad9fa4c5b71cefc1e44502521e15eb13944def

--- assets/README.md.BIOB2LWM.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.Sr95JMjc.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md","lastUpdated":1777503149000}'),r={name:"README.md"};function o(s,c,n,d,p,_){return a(),t("div")}const E=e(r,[["render",o]]);export{m as __pageData,E as default};
